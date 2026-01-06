302505c35c07e0aeed6d7c7c73fb0150b72ef184
supremeleadoor <supremeleadoor@gmail.com>
2026-01-06 16:19:37 +0530

    Update heroes.js

--- api/heroes.js
+++ api/heroes.js
@@ -9,8 +9,9 @@ const api = Client.getInstance(config);
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Max-Age', '86400');
   
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
